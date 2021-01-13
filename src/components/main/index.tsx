fa76efaa013ec456947a4151bc7613dfa5883c64
vinicius.paixao <vinicius.paixao@acct.global>
2021-01-13 20:24:59 -0300

    testing library configuration

---
+++ src/components/main/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// import { Container } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <main>
+      <h2>React Avançado</h2>
+    </main>
+  )
+}
+
+export default Main
