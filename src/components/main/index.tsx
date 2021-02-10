bdfab6af99d83de71274b4f611997f881df6819b
vinicius.paixao <vinicius.paixao@acct.global>
2021-02-10 19:59:09 -0300

    first styled components and template

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import * as S from './styles'
 
 const Main = () => {
   return (
-    <>
-      <h2>React Avançado</h2>
-    </>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um atomo e React Avançado escrito ao lado."
+      />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        TypeScript, ReactJS, NextJS, e Styled Components
+      </S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="um desenvolvedor de frente para uma tela com codigo."
+      />
+    </S.Wrapper>
   )
 }
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,16 +4,16 @@ const GlobalStyles = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
-    box-sizing: border-box
+    box-sizing: border-box;
   }
   html {
     font-size: 62.5%;
   }
   html, body, #__next {
-    height: 100%
+    height: 100%;
   }
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 `
 export default GlobalStyles
