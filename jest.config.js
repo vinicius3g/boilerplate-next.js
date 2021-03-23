790f3ae61b370c5d98de72b8c74287d18f86b86f
vinicius.paixao <vinicius.paixao@acct.global>
2021-03-23 19:54:02 -0300

    jest adjusts

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnviroment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/','/.next/'],
-  collectCoverage:true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv:['<rootDir>/.jest/setup.ts']
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
