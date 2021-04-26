4112dfc6ef4136ddcc69757ed1a7670e28fb1b66
vinicius.paixao <vinicius.paixao@acct.global>
2021-04-26 16:46:55 -0300

    adjusts

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,9 @@
+import Button from '.'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+export default {
+  title: 'Button',
+  component: Button
+} as Meta
+
+export const Basic: Story = () => <Button />
---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from './styles'
+
+const Logo = () => {
+  return (
+    <S.Wrapper>
+      <h1>Logo</h1>
+    </S.Wrapper>
+  )
+}
+
+export default Logo
